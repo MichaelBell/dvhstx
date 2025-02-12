cf1a1b39b6dad8fa5c45fe17d56242bd6381d17d
mattuna15 <matthew.pearce@sky.uk>
2025-02-12 19:42:14 +0000

    adafruit rp2350

--- pico_dvhstx.cmake
+++ pico_dvhstx.cmake
@@ -1,4 +1,33 @@
+# Generated Cmake Pico project file
 
+cmake_minimum_required(VERSION 3.13)
+
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+# Initialise pico_sdk from installed location
+# (note this can come from environment, CMake cache etc)
+
+# == DO NOT EDIT THE FOLLOWING LINES for the Raspberry Pi Pico VS Code Extension to work ==
+if(WIN32)
+    set(USERHOME $ENV{USERPROFILE})
+else()
+    set(USERHOME $ENV{HOME})
+endif()
+set(sdkVersion 2.1.0)
+set(toolchainVersion 13_3_Rel1)
+set(picotoolVersion 2.1.0)
+set(picoVscode ${USERHOME}/.pico-sdk/cmake/pico-vscode.cmake)
+if (EXISTS ${picoVscode})
+    include(${picoVscode})
+endif()
+# ====================================================================================
+set(PICO_BOARD adafruit_feather_rp2350 CACHE STRING "Board type")
+
+# Pull in Raspberry Pi Pico SDK (must be before project)
+include(pico_sdk_import.cmake)
+include(pimoroni_pico_import.cmake)
 include(drivers/dvhstx/dvhstx)
 
 set(LIB_NAME pico_dvhstx)
